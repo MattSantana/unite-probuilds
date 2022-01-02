dbd802204bbf71dace6883212ea5124dc7cba793
Leonardo Araujo <leoujooficial@gmail.com>
2022-01-02 18:36:35 -0300

    web app fully api integrated

--- src/Components/CardBuild/CardBuild.jsx
+++ src/Components/CardBuild/CardBuild.jsx
@@ -5,26 +5,39 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import "./CardBuild.css";
+import DialogItem from "../Dialogs/DialogItem";
 
 export default function CardBuild(props) {
+  const [dialogItem, setDialogItem] = React.useState(null);
+  const [dialogAttack, setDialogAttack] = React.useState(null);
+
+  const handleCloseDialog = () => {
+    setDialogItem(null);
+    setDialogAttack(null);
+  };
+
   return (
     <Card sx={{ maxWidth: 700, marginTop: "1em" }}>
       <CardActionArea>
         <CardMedia component="img" height="180" img src={props.build.image} />
 
         <CardContent sx={{ backgroundColor: "#fe8b25" }}>
+          <DialogItem dialogData={dialogItem} handleCloseDialog={handleCloseDialog} />
+          <DialogItem dialogData={dialogAttack} isAttack handleCloseDialog={handleCloseDialog} />
           <div style={{ display: "flex", justifyContent: "space-between", flexDirection: "column" }}>
             <div style={{ display: "flex", justifyContent: "flex-start" }}>
               {/*ítens */}
               {props.build.items.map((item, key) => (
-                <img key={key} src={item.image} alt="" style={{ width: "60px", height: "60px", margin: "8px " }} />
+                <>
+                  <img key={key} src={item.image} alt="item" style={{ width: "60px", height: "60px", margin: "8px " }} onClick={() => setDialogItem(item)} />
+                </>
               ))}
             </div>
             <br />
             <div style={{ display: "flex", justifyContent: "flex-start" }}>
               {/*ataques */}
               {props.build.attacks.map((attack, key) => (
-                <img key={key} src={attack.image} alt="" style={{ width: "60px", height: "60px", margin: "8px " }} />
+                <img key={key} src={attack.image} alt="attack" style={{ width: "60px", height: "60px", margin: "8px " }} onClick={() => setDialogAttack(attack)} />
               ))}
             </div>
           </div>
---
+++ src/Components/Dialogs/DialogItem.jsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+}));
+
+const BootstrapDialogTitle = (props) => {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+};
+
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default function DialogTip(props) {
+  return (
+    <>
+      <BootstrapDialog aria-labelledby="customized-dialog-title" open={props.dialogData !== null}>
+        <BootstrapDialogTitle id="customized-dialog-title" onClose={props.handleCloseDialog}>
+          {props.dialogData?.name}
+        </BootstrapDialogTitle>
+        <DialogContent dividers style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <img style={{ width: "60px", height: "60px", marginRight: "1em" }} src={props.dialogData?.image} alt="" />
+          <Typography gutterBottom>{props.dialogData?.description}</Typography>
+        </DialogContent>
+        <DialogContent dividers style={{ display: "flex", justifyContent: "space-evenly" }}>
+          {props.isAttack ? (
+            <>
+              <Typography gutterBottom>Reload Time: {props.dialogData?.reloadTime}</Typography>
+            </>
+          ) : (
+            <>
+              <Typography gutterBottom>Atributos:</Typography>
+
+              {props.dialogData?.atributtes?.map((atributte) => (
+                <Typography gutterBottom>
+                  {atributte.type}: {atributte.amount}
+                </Typography>
+              ))}
+            </>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={props.handleCloseDialog}>
+            Entendi
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+    </>
+  );
+}
